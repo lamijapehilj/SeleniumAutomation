45351f5b6eda5491ac6ac7415ee2283c11859bcd
Lamija Pehilj <lamija.pehilj@gmail.com>
2026-03-01 17:47:13 +0100

    Initial commit - Selenium C# project

---
+++ CSharpFundas/Program2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharpFundas;
+
+
+class SecondProgram
+{
+    static void Run()
+    {
+        Console.WriteLine("I am in the second program");
+
+        Program p = new Program();
+        p.getData();
+
+    }
+}
---
+++ CSharpFundas/Program5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+
+//
+String[] a = { "hello", "bye", "Rahul", "Shetty" };
+int[] b = { 1, 2, 3, 4, 5 };
+
+string[] a1 = new string[4];
+a1[0] = "hello";
+a1[1] = "bye";
+
+Console.WriteLine(a[1]);
+
+for (int i = 0; i < a.Length; i++)
+{
+    Console.WriteLine(a[i]);
+    if (a[i] == "Rahul")
+    {
+        Console.WriteLine("Match found");
+        break;
+    }
+    
+}
