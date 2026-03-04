63f09919a0e89c144097553d2a221f93d414fecb
Lamija Pehilj <lamija.pehilj@gmail.com>
2026-03-04 22:28:08 +0100

    My commit

--- SeleniumLearning/SeleniumFirst.cs
+++ SeleniumLearning/SeleniumFirst.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace SeleniumLearning
 {
-    internal class SeleniumFirst
+    public class SeleniumFirst
     {
+        IWebDriver driver;
+  
+
+       [SetUp]
+
+        public void StartBrowser()
+        {
+            //Methods - geturl,click
+            //chromedriver.exe on chrome browser
+            //edgedriver.exe
+            //geckodriver (Firefox .exe)
+            //99.exe fro existing chrome browser (99)
+            //WebDriverManager-(
+            // new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            //driver = new ChromeDriver();
+            //new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+            new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+
+            //driver = new FirefoxDriver();
+            driver = new EdgeDriver();
+
+            
+            driver.Manage().Window.Maximize();
+            
+        }
+
+        [Test]
+
+        public void Test1()
+        {
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/"; //setting the property
+            TestContext.Progress.WriteLine(driver.Title);
+            TestContext.Progress.WriteLine(driver.Url);  //getting the property
+            
+            
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+            //driver.Quit(); //2 windows, quit all windows
+        }
     }
+
 }
